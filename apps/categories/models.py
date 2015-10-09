41c6b4246e2f95a9c6ae7cf144c18e584bbcbaae
Hoang Vuong <hoang.van.vuong@framgia.com>
2015-10-09 10:14:54 +0700

    Adding rating field for Review model

--- apps/categories/models.py
+++ apps/categories/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(blank=False, max_length=257)
+    name = models.CharField(blank=False, max_length=255)
     description = models.TextField(blank=True, default='')
 
     class Meta:
--- apps/reviews/models.py
+++ apps/reviews/models.py
@@ -5,10 +5,18 @@ from apps.books.models import Book
 
 
 class Review(models.Model):
+    RATING_STARS = (
+            (1, 'One star'),
+            (2, 'Two stars'),
+            (3, 'Three stars'),
+            (4, 'Four stars'),
+            (5, 'Five stars'),
+    )
     user_profile = models.ForeignKey(UserProfile)
     book = models.ForeignKey(Book)
     content = models.TextField(blank=True, default='')
     updated_time = models.DateTimeField(auto_now=True)
+    rating = models.IntegerField(choices=RATING_STARS, default=4)
 
     class Meta:
         db_table = 'review'
