2a57f6ba0c9386addc7ad448cfcacb60ba64808e
Hoang Vuong <hoang.van.vuong@framgia.com>
2015-10-09 15:13:41 +0700

    Adding templates for login, logout, and signup users

--- apps/books/urls.py
+++ apps/books/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.HomePageView.as_view(), name='list_book'),
+	url(r'^$', views.HomePageView.as_view(), name='index'),
 ]
