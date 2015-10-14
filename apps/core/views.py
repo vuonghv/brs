5db32318faea752df6856f6370a2dbebdcff15b1
Hoang Vuong <hoang.van.vuong@framgia.com>
2015-10-14 11:02:03 +0700

    [FIX] Fix list_recommendations_book

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,7 +1,7 @@
 from django.views.generic.detail import ContextMixin
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
-from django.db.models import Avg
+from django.db.models import Avg, Count
 
 from apps.categories.models import Category
 from apps.books.models import Book, UserProfileBook
@@ -13,8 +13,10 @@ class BaseView(ContextMixin):
     def get_context_data(self, **kwargs):
         context = super(BaseView, self).get_context_data(**kwargs)
         info = {
-            'list_book_recommendations': Book.objects.order_by('-favourites', '-id')[0:5],
-            'list_top_review': Book.objects.annotate(Avg('review__rating')).order_by('-review__rating__avg')[0:5],
+            'list_book_recommendations': Book.objects.annotate(
+                Count('favourites')).order_by('-favourites__count')[0:5],
+            'list_top_review': Book.objects.annotate(
+                Avg('review__rating')).order_by('-review__rating__avg')[0:5],
             'list_category': Category.objects.all()
         }
         context.update(info)
