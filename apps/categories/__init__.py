32dc9af33e4def805dce89fc81f83a1d8cc29123
Hoang Vuong <hoang.van.vuong@framgia.com>
2015-10-14 10:50:54 +0700

    [FIX] Fix list_top_review

---
+++ apps/categories/__init__.py
@@ -0,0 +1,3 @@
+"""
+This app supports some utils for the project.
+"""
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,6 +1,7 @@
 from django.views.generic.detail import ContextMixin
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
+from django.db.models import Avg
 
 from apps.categories.models import Category
 from apps.books.models import Book, UserProfileBook
@@ -13,7 +14,7 @@ class BaseView(ContextMixin):
         context = super(BaseView, self).get_context_data(**kwargs)
         info = {
             'list_book_recommendations': Book.objects.order_by('-favourites', '-id')[0:5],
-            'list_top_review': Review.objects.order_by('-rating', '-updated_time')[0:5],
+            'list_top_review': Book.objects.annotate(Avg('review__rating')).order_by('-review__rating__avg')[0:5],
             'list_category': Category.objects.all()
         }
         context.update(info)
