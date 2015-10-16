ebee2af6b8efa589ba85cbf06c9a09c1209dffec
Hoang Vuong <hoang.van.vuong@framgia.com>
2015-10-16 08:12:20 +0700

    Settings for storing media files in AWS S3

--- brs/settings/prod.py
+++ brs/settings/prod.py
@@ -1,5 +1,6 @@
 import dj_database_url
 from brs.settings.base import *
+from brs import custom_storages   
 
 DEBUG = False
 
@@ -16,6 +17,8 @@ THIRD_APPS = [
 
 INSTALLED_APPS += THIRD_APPS
 
+# For Amazon Web Services settings
+
 AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
@@ -24,6 +27,18 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 
 AWS_S3_CUSTOM_DOMAIN = '{}.s3.amazonaws.com'.format(AWS_STORAGE_BUCKET_NAME)
 
-STATIC_URL = 'https://{}/'.format(AWS_S3_CUSTOM_DOMAIN)
+# For static files
 
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+STATICFILES_LOCATION = 'static'
+
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+
+STATIC_URL = 'https://{}/{}/'.format(AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+
+# For media files
+
+MEDIAFILES_LOCATION = 'media'
+
+MEDIA_URL = 'https://{}/{}/'.format(AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
