c6a04e8ee5cdce6242b24388e317e9c5fec80263
Hoang Vuong <hoang.van.vuong@framgia.com>
2015-10-19 11:42:21 +0700

    Removing the duplicated contexts request

--- brs/settings/base.py
+++ brs/settings/base.py
@@ -88,9 +88,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-
-                # `allauth` needs this from django
-                'django.template.context_processors.request',
             ],
         },
     },
